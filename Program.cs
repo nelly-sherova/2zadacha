43c82210ef6d9cbfd4642c2fa04e914a55f7a9a1
unknown <lvovanikol@mail.ru>
2020-03-22 11:45:02 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
