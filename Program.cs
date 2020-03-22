fa4e36b45d9832d484a6f57fd85050db8246795c
unknown <lvova.nikol@mail.ru>
2020-03-22 12:36:42 +0300

    created console .net core project

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace _2zadanie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 2 задача
+            Console.Write("A = ");
+            decimal a = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("B = ");
+            decimal b = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("C = ");
+            decimal c = Convert.ToDecimal(Console.ReadLine());
+            decimal AC, BC;
+            AC = Math.Abs(a - c);
+            BC = Math.Abs(b - c);
+            Console.WriteLine($"Длина отрезка AC =  {AC}");
+            Console.WriteLine($"Длина отрезка BC =  {BC}");
+            Console.WriteLine($"AC+BC = {AC + BC} ");
         }
     }
 }
